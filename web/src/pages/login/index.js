d010005767a8acf6bcf7b84a4b317e60d479ad2b
Kamron Erani <kamronerani@gmail.com>
2017-10-06 12:24:49 -0700

    Add login page and use sass preprocessor
    Change create-react-app to use a sass preprocessor to be able to customize bulma, also changed the primary, info and radius variables in the bulma file.

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -3,17 +3,26 @@ import React, { Component } from 'react';
 class LoginPage extends Component {
     render() {
         return (
-            <section className="hero is-fullheight">
+            <section className="hero is-info is-fullheight">
                 <div className="hero-body">
                     <div className="container">
                         <div className="columns">
-                            <div className="box">
-                                <div className="field">
-                                    <div className="control">
-                                        <input className="input" type="text" placeholder="ucinetid" />
+                            <div className="column is-4 is-offset-4">
+                                <div className="box">
+                                    <div className="field">
+                                        <div className="control">
+                                            <label className="label">UCINETID</label>
+                                            <input className="input" type="text" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <label className="label">PASSWORD</label>
+                                            <input className="input" type="password" />
+                                        </div>
                                     </div>
                                     <div className="control">
-                                        <input className="input" type="password" placeholder="password" />
+                                        <button className="button is-primary is-large is-fullwidth">Login</button>
                                     </div>
                                 </div>
                             </div>
