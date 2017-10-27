2d782bf22e309f497610dbfd38358291fb056669
Kamron Erani <kamronerani@gmail.com>
2017-10-26 21:23:12 -0700

    Add course offerings to courseDetails and modify getCourseDetails
    The new getCourseDetails also gets all the offerings of the course by searching the schedule.

--- web/src/components/CourseDetails/index.js
+++ web/src/components/CourseDetails/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PreReqs from '../PreReqs';
 import GradeDistribution from '../GradeDistribution';
+import CourseOfferings from '../SOC/CourseOfferings';
 
 class CourseDetails extends Component {
     render() {
@@ -10,10 +11,15 @@ class CourseDetails extends Component {
                 <h5 className="title is-5">{this.props.course.dept} {this.props.course.num}</h5>
                 <p>{this.props.course.description}</p>
                 <div className="is-divider"></div>
-                <PreReqs prereqs={this.props.course.prereqs} coursesTaken={Object.assign(this.props.user.courses.inProgress, this.props.user.courses.completed)}/>
+                <PreReqs prereqs={this.props.course.prereqs} coursesTaken={Object.assign(this.props.user.courses.inProgress, this.props.user.courses.completed)} />
                 <div className="is-divider"></div>
                 <GradeDistribution grades={this.props.course.gradeDistributions} />
                 <div className="is-divider"></div>
+                {this.props.course.offerings.length === 0 ?
+                    <p>No Course Offerings :(</p>
+                    :
+                    <CourseOfferings offerings={this.props.course.offerings} addPlannedCourse={this.props.addPlannedCourse} />
+                }
             </div>
         )
     }
