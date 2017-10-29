6722a9080820ab356d839e27bd5c249b924ed449
Kamron Erani <kamronerani@gmail.com>
2017-10-29 01:03:47 -0700

    Add bookmarklet login instead of extension
    Until the chrome extension has more features than just logging in, replace it with a bookmarklet and instructions when they press Login Through WebAuth.

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import MessageBox from '../../components/MessageBox';
+import './style.css';
+
 class LoginPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             ucinetid: '',
-            password: ''
+            password: '',
+        }
+    }
+
+    componentDidMount = () => {
+        if (global.location.search.includes('?ucinetid_auth=')) {
+            this.login(null, global.location.search.replace('?ucinetid_auth=', ''));
         }
     }
 
@@ -21,6 +29,35 @@ class LoginPage extends Component {
             ucinetid: this.state.ucinetid,
             password: this.state.password
         })
+    }
+
+    showWebAuthModal = (showWebAuthModal) => {
+        this.setState({
+            showWebAuthModal
+        })
+    }
+
+    loginThroughWebAuthModal = () => {
+        return (
+            <div class="modal is-active">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login Through WebAuth</p>
+                        <button class="delete" aria-label="close" onClick={() => { this.showWebAuthModal(false) }}></button>
+                    </header>
+                    <section class="modal-card-body">
+                        <p style={{ color: "black", marginBottom: 10 }}>Logging into MyrmeyAssist through WebAuth is easy! In order to do it you will have to drag the blue MyrmeyAssist button below
+                            into your bookmarks bar and when you are ready, click it. It will redirect you to WebAuth where you will have to login.
+                            Once logged in you can press the bookmark again and you will be logged into MyrmeyAssist. This will also work if you are
+                            logged into any of UCI's services.</p>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <a className="button is-primary is-large" href="javascript:void%20function(){uciRegex=/ucinetid_auth=(%3F!no_key)(.+)/.exec(document.cookie),uciRegex%3Fwindow.location.href=%22http://localhost:3000/webauth%3Fucinetid_auth=%22+uciRegex[1]:(window.location.href=%22https://login.uci.edu/ucinetid/webauth%22,alert(%22Login%20to%20WebAuth.%20Once%20logged%20in,%20press%20the%20MyrmeyAssist%20bookmark%20again!%22))}()">MyrmeyAssist</a>
+                    </footer>
+                </div>
+            </div>
+        )
     }
 
     render() {
@@ -52,12 +89,8 @@ class LoginPage extends Component {
                                             </div>
                                         </div>}
                                     <div className="control">
-                                        {true ?//global.chrome.app.isInstalled ?
-                                            [<input key={1} type="hidden" id="ucinetid_auth" onClick={(e) => { this.login(e, e.target.value) }} />,
-                                            <a key={2} id="webauth_login" className={'button is-primary is-outlined is-medium is-fullwidth ' + (this.props.loginStatus.loggingIn && 'is-loading')} href="https://login.uci.edu/ucinetid/webauth?return_url=http://localhost:3000/webauth">Login Through WebAuth </a>]
-                                            :
-                                            <button className={'button is-primary is-outlined is-medium is-fullwidth ' + (this.props.loginStatus.loggingIn && 'is-loading')} onClick={() => { global.chrome.webstore.install() }}>Enable WebAuth Login</button>
-                                        }
+                                        <input type="hidden" id="ucinetid_auth" onClick={(e) => { this.login(e, e.target.value) }} />
+                                        <a id="webauth_login" className={'button is-primary is-outlined is-medium is-fullwidth ' + (this.props.loginStatus.loggingIn && 'is-loading')} onClick={() => {this.showWebAuthModal(true)}}>Login Through WebAuth </a>
                                     </div>
                                 </div>
                                 {this.props.loginStatus.error !== undefined && <MessageBox type="is-danger" message={this.props.loginStatus.error} />}
@@ -70,6 +103,7 @@ class LoginPage extends Component {
                         <p>Disclaimer: Logging into Project Myrmey results in your ucinetid and password being stored in plain text on an insecure server in Malaysia. Use at your own risk.</p>
                     </div>
                 </div>
+                {this.state.showWebAuthModal && this.loginThroughWebAuthModal()}
             </section>
         );
     }
