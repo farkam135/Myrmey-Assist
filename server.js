e1c57b59bf61f23c92689566e17a445dbd93267d
Kamron Erani <kamronerani@gmail.com>
2017-11-11 21:46:22 -0800

    Move production bool to config.json and don't use greenlock for dev

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const config = require('./config.json');
 
-const PROD = false;
+let serverInitiated = false;
 
 const services = {}
 if (config.watchlist.enable) {
@@ -200,6 +200,14 @@ app.get('/api/getCourseDetails', (req, res) => {
 });
 
 app.post('/api/searchSchedule', (req, res) => {
+    if(!serverInitiated){
+        res.send({
+            success: false,
+            error: 'MyrmeyAssist was just updated and is currently restarting. Please try again in 1-2 minutes.'
+        })
+        return;
+    }
+
     console.log(`[searchSchedule REQUEST]`);
     if (req.body.InstrName === '' && req.body.CourseCodes === '' && req.body.Dept === 'ALL' && req.body.Breadth === 'ANY') {
         res.send({
@@ -249,14 +257,14 @@ app.post('/api/addWatchlist', (req, res) => {
         return;
     }
     MYRMEYDB.addWatch(req.body.email, req.body.code)
-    .then(() => {
-        res.send({ success: true });
-    })
-    .catch((err) => {
-        if (err.detail.includes('already exists')) {
-            res.send({ success: false, error: `${req.body.code} is already on your watchlist.` });
-        }
-    });
+        .then(() => {
+            res.send({ success: true });
+        })
+        .catch((err) => {
+            if (err.detail.includes('already exists')) {
+                res.send({ success: false, error: `${req.body.code} is already on your watchlist.` });
+            }
+        });
 
     //Disabled while login is disabled
     /* if (!req.body.myrmeyid || !req.body.email || !req.body.code) {
@@ -287,10 +295,6 @@ app.post('/api/addWatchlist', (req, res) => {
                 }
             });
     }); */
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web/build/index.html'));
 });
 
 function approveDomains(opts, certs, cb) {
@@ -315,37 +319,47 @@ function approveDomains(opts, certs, cb) {
     cb(null, { options: opts, certs: certs });
 }
 
+function startProdServer() {
+    // returns an instance of node-greenlock with additional helper methods
+    var lex = require('greenlock-express').create({
+        // set to https://acme-v01.api.letsencrypt.org/directory in production
+        server: 'https://acme-v01.api.letsencrypt.org/directory'
+
+        // If you wish to replace the default plugins, you may do so here
+        //
+        , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+        , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+
+        // You probably wouldn't need to replace the default sni handler
+        // See https://git.daplie.com/Daplie/le-sni-auto if you think you do
+        //, sni: require('le-sni-auto').create({})
+
+        , approveDomains: approveDomains
+    });
+
+    // handles acme-challenge and redirects to https
+    require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+        console.log("Listening for ACME http-01 challenges on", this.address());
+    });
+
+    // handles your app
+    require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+        console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+    });
+}
+
+if (config.production) {
+    startProdServer();
+}
+else {
+    app.listen(8080);
+}
 
 UCI.SOC.init()
     .then(() => {
         //return Promise.resolve();
-        return Promise.all([UCI.PROFS.refreshProfs(), UCI.SOC.loadAll()]);
+        return Promise.all([UCI.PROFS.refreshProfs(), UCI.SOC.loadDept("COMPSCI")]);
     })
     .then(() => {
-        // returns an instance of node-greenlock with additional helper methods
-        var lex = require('greenlock-express').create({
-            // set to https://acme-v01.api.letsencrypt.org/directory in production
-            server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging'
-
-            // If you wish to replace the default plugins, you may do so here
-            //
-            , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-            , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
-
-            // You probably wouldn't need to replace the default sni handler
-            // See https://git.daplie.com/Daplie/le-sni-auto if you think you do
-            //, sni: require('le-sni-auto').create({})
-
-            , approveDomains: approveDomains
-        });
-
-        // handles acme-challenge and redirects to https
-        require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-            console.log("Listening for ACME http-01 challenges on", this.address());
-        });
-
-        // handles your app
-        require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
-            console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-        });
+        serverInitiated = true;
     })
