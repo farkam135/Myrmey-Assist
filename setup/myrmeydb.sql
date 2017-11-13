76f67fa68b4138ccac844d2480ebab1ddaa17ecb
Kamron Erani <farkam135@users.noreply.github.com>
2017-11-12 23:55:00 -0800

    Update myrmeydb.sql

--- setup/myrmeydb.sql
+++ setup/myrmeydb.sql
@@ -16,14 +16,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 2178 (class 0 OID 0)
--- Dependencies: 1
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
 
 SET search_path = public, pg_catalog;
 
